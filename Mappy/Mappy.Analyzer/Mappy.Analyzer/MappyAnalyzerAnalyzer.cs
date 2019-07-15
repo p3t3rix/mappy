680a33fc47c0c1ef7051d174a71106d08a44f2b1
peterix <peterkahpunkt@gmail.com>
2019-07-15 15:27:23 +0200

    cleanup

--- Mappy/Mappy.Analyzer/Mappy.Analyzer/MappyAnalyzerAnalyzer.cs
+++ Mappy/Mappy.Analyzer/Mappy.Analyzer/MappyAnalyzerAnalyzer.cs
@@ -20,13 +20,11 @@ namespace Mappy.Analyzer
         /// <summary>
         /// We cant use nameof/typeof of a referenced library becuase then we would need to do voodoo magic with nuget. So as it stands we use hardcoded strings and hope for the best
         /// </summary>
-        private static string _mappingAttributeFullName = "Mappy.Core.WarnIfNotMappedCompletelyAttribute";
-        //private static string _mappingAttributeFullName = typeof(WarnIfNotMappedCompletelyAttribute).FullName;
 
         private static string _mappingAttributeName = "WarnIfNotMappedCompletelyAttribute";
         //private static string _mappingAttributeName = nameof(WarnIfNotMappedCompletelyAttribute);
 
-        private static string _mappingAttributeWithoutAttributeSuffix= "WarnIfNotMappedCompletely";
+        private static string _mappingAttributeWithoutAttributeSuffix = "WarnIfNotMappedCompletely";
         //private static string _mappingAttributeWithoutAttributeSuffix= nameof(WarnIfNotMappedCompletelyAttribute).Replace("Attribute","");
 
         private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Description, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
@@ -45,7 +43,7 @@ namespace Mappy.Analyzer
             foreach (var tree in compilation.SyntaxTrees)
             {
                 var semanticModel = compilation.GetSemanticModel(tree);
-                var result = AnalyzeForMappings(semanticModel, compilation);
+                var result = AnalyzeForMappings(semanticModel);
                 foreach (var item in result)
                 {
                     context.ReportDiagnostic(CreateDiagnostic(item));
@@ -56,8 +54,7 @@ namespace Mappy.Analyzer
         private static void AnalyzeSemantic(SemanticModelAnalysisContext context)
         {
             SemanticModel model = context.SemanticModel;
-            Compilation compilation = model.Compilation;
-            var result = AnalyzeForMappings(model, compilation);
+            var result = AnalyzeForMappings(model);
             foreach (var item in result)
             {
                 context.ReportDiagnostic(CreateDiagnostic(item));
@@ -68,66 +65,87 @@ namespace Mappy.Analyzer
             return Diagnostic.Create(Rule, item.Key.Locations[0], string.Join(", ", item.Value));
         }
 
-        private static Dictionary<ISymbol, HashSet<string>> AnalyzeForMappings(SemanticModel model, Compilation compilation)
+        private static Dictionary<ISymbol, HashSet<string>> AnalyzeForMappings(SemanticModel model)
         {
             var result = new Dictionary<ISymbol, HashSet<string>>();
-            var attribute = compilation.GetTypeByMetadataName(_mappingAttributeFullName);
 
-            var methodDeclarations = model.SyntaxTree.GetRoot().DescendantNodes().OfType<MethodDeclarationSyntax>();
-            var methods = methodDeclarations
-                .Where(md => md.AttributeLists
-                    .Any(al => al.Attributes
-                        .Any(a => (a.Name as IdentifierNameSyntax).Identifier.Text == _mappingAttributeWithoutAttributeSuffix)
-                        )
-                    );
+            var methods = GetRelevantMethodsFromModel(model);
 
             foreach (var methodSyntax in methods)
             {
-                var method = model.GetDeclaredSymbol(methods.First());
-                var mappingAttribute = method.GetAttributes().FirstOrDefault(a => a.AttributeClass.MetadataName == _mappingAttributeName);
-                List<string> ignoredMembers = ExtractIgnoredMembers(mappingAttribute);
+                var method = model.GetDeclaredSymbol(methodSyntax);
+                var ignoredMembers = ExtractIgnoredMembersFromAttribute(method);
 
-                var targetTypeArguments = method.Parameters.Where(p => p.Type == method.ReturnType);
-                if (targetTypeArguments.Any())
+                var targetArgument = GetTargetArgumentFromMethod(method);
+
+                if (targetArgument == null)
+                    continue;
+
+                var methodResult = AnalyzeMethodBody(methodSyntax, ignoredMembers, targetArgument).ToList();
+
+                if (methodResult.Any())
                 {
-                    var targetTypeArgument = targetTypeArguments.Last();
-                    var targetPropertyName = targetTypeArgument.Name.ToString();
-                    var notMappedMembers = InitializeNotMappedMembers(ignoredMembers, targetTypeArgument);
-
-                    foreach (var statement in methodSyntax.Body.Statements.OfType<ExpressionStatementSyntax>())
+                    if (!result.ContainsKey(method))
                     {
-                        var expr = statement.Expression;
-                        if (expr is AssignmentExpressionSyntax assignment)
-                        {
-                            if (assignment.Left is MemberAccessExpressionSyntax ma)
-                            {
-
-                                var sourceName = (ma.Expression as IdentifierNameSyntax)?.Identifier.Text;
-                                var propertyName = ma?.Name?.Identifier.Text;
-                                if (string.Equals(sourceName, targetPropertyName))
-                                {
-                                    notMappedMembers = notMappedMembers.Where(m => m != propertyName);
-                                }
-                            }
-                        }
+                        result.Add(method, new HashSet<string>());
                     }
-                    if (notMappedMembers.Any())
-                    {
-                        if (!result.ContainsKey(method))
-                        {
-                            result.Add(method, new HashSet<string>());
-                        }
 
-                        foreach (var item in notMappedMembers)
-                        {
-                            result[method].Add(item);
-                        }
+                    foreach (var item in methodResult)
+                    {
+                        result[method].Add(item);
                     }
                 }
             }
 
             return result;
 
+        }
+
+        private static IEnumerable<string> AnalyzeMethodBody(MethodDeclarationSyntax methodSyntax, List<string> ignoredMembers, IParameterSymbol targetArgument)
+        {
+            var targetPropertyName = targetArgument.Name;
+            var notMappedMembers = InitializeNotMappedMembers(ignoredMembers, targetArgument);
+
+            foreach (var statement in methodSyntax.Body.Statements.OfType<ExpressionStatementSyntax>())
+            {
+                var expr = statement.Expression;
+
+                if (!(expr is AssignmentExpressionSyntax assignment)) continue;
+                if (!(assignment.Left is MemberAccessExpressionSyntax ma)) continue;
+
+                var sourceName = (ma.Expression as IdentifierNameSyntax)?.Identifier.Text;
+                var propertyName = ma.Name?.Identifier.Text;
+                if (string.Equals(sourceName, targetPropertyName))
+                {
+                    notMappedMembers = notMappedMembers.Where(m => m != propertyName);
+                }
+            }
+            return notMappedMembers;
+        }
+
+        private static IParameterSymbol GetTargetArgumentFromMethod(IMethodSymbol method)
+        {
+            var targetTypeArguments = method.Parameters.Where(p => Equals(p.Type, method.ReturnType)).ToList();
+            return targetTypeArguments.Any() ? targetTypeArguments.Last() : null;
+        }
+
+        private static List<string> ExtractIgnoredMembersFromAttribute(IMethodSymbol method)
+        {
+            var attribute = method.GetAttributes().FirstOrDefault(a => a.AttributeClass.MetadataName == _mappingAttributeName);
+            return ExtractIgnoredMembers(attribute);
+        }
+
+        private static List<MethodDeclarationSyntax> GetRelevantMethodsFromModel(SemanticModel model)
+        {
+            var methodDeclarations = model.SyntaxTree.GetRoot().DescendantNodes().OfType<MethodDeclarationSyntax>();
+
+            return methodDeclarations
+                .Where(md => md.AttributeLists
+                    .Any(al => al.Attributes
+                        .Any(a => (a.Name as IdentifierNameSyntax)?.Identifier.Text == _mappingAttributeWithoutAttributeSuffix)
+                        )
+                    )
+                .ToList();
         }
 
         private static IEnumerable<string> InitializeNotMappedMembers(List<string> ignoredMembers, IParameterSymbol targetTypeArgument)
